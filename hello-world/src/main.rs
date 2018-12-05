28b6569bab9b44764f030f2d1888610449547d0b
Ayush Prashar <ayush.prashar@gmail.com>
2018-12-05 17:39:02 +0530

    Maintaining state

--- hello-world/src/main.rs
+++ hello-world/src/main.rs
@@ -1,16 +1,25 @@
 extern crate actix_web;
 
-use actix_web::{ server, App, HttpRequest };
-
-fn index(req: &HttpRequest) -> &'static str {
-	"Ayush Prashar"
+use actix_web::{ server, App, http, HttpRequest };
+use std::cell::Cell;
+struct AppState {
+	counter: Cell<usize>,
 }
+
+fn index(req: &HttpRequest<AppState>) -> String {
+	let count = req.state().counter.get() + 1;
+	req.state().counter.set(count);
+	format!("The state is {}", count)
+}
+
 fn main() {
-    server::new(
-    		|| App::new()
-    			.resource("/", |r| r.f(index)))
-    			.bind("127.0.0.1:8088")
-    			.unwrap()
-    			.run();
+    server::new( || {
+    	App::with_state( AppState {
+    		counter: Cell::new(0)
+    	}).resource("/", |r| r.method(http::Method::GET).f(index))
+    })
+    		.bind("127.0.0.1:8088")
+    		.unwrap()
+    		.run();
     		
 }
